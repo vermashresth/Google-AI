3a67884337796e64d6e277d03854e2761db6e06c
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-09-01 11:08:36 +0000

    Update get_data.sh

--- mmitra-rmab/get_data.sh
+++ mmitra-rmab/get_data.sh
@@ -5,12 +5,12 @@ DATE=$3
 ## Yet to finalise, clarifications need from online database 
 
 # Get Beneficiary Data
-echo "SELECT u.beneficiary_id user_id, isactive, MD5(phone_no) phone_no, lmp_date lmp, enrollment_gestation_age enroll_gest_age, u.project_id, u.call_slot_id call_slots, enrollment_delivery_status enroll_delivery_status,
+echo "SELECT u.beneficiary_id user_id, isactive, phone_no, lmp_date lmp, enrollment_gestation_age enroll_gest_age, u.project_id, u.call_slot_id call_slots, enrollment_delivery_status enroll_delivery_status,
 language_id LANGUAGE, registration_date, delivery_date, entry_date, phone_type, phone_code, phone_owner,
 u.channel_id ngo_hosp_id, CASE c.channel_type WHEN 1 THEN 'Community' WHEN 2 THEN 'Hospital' ELSE 'ARMMAN' END AS ChannelType,
 unique_id unique_sub_id, entry_madeby, entry_updatedby,
 forced_delivery_update force_delivery_updated, completed,  dnd_optout_status, age, education_id education,
-MD5(alternate_phone_no) alternate_no, alternate_phone_owner alternate_no_owner, name_of_sakhi, name_of_project_officer, income_bracket, data_entry_officer,
+alternate_phone_no, alternate_phone_owner alternate_no_owner, name_of_sakhi, name_of_project_officer, income_bracket, data_entry_officer,
 g, p, s, l, a, ppc_bloodpressure, ppc_diabetes, ppc_cesarean, ppc_thyroid,  ppc_fibroid, ppc_spontaneousAbortion, ppc_heightLess140,
 ppc_pretermDelivery, ppc_anaemia, ppc_otherComplications, name_of_medication_any, planned_place_of_delivery, registered_where, registered_pregnancy,
 place_of_delivery, type_of_delivery, date_registration_hospital, term_of_delivery, medication_after_delivery
