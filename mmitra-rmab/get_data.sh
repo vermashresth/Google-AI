756a6b58841b663a7a65ac62ee4da8b83c28a941
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-09-01 14:50:32 +0530

    call query update

--- mmitra-rmab/get_data.sh
+++ mmitra-rmab/get_data.sh
@@ -22,7 +22,7 @@ ORDER BY u.id;" | /google/data/ro/projects/speckle/mysql -h 34.93.237.61 -P 3306
 sed -i 's/LANGUAGE/language/' data/beneficiary/beneficiary_pilot_data.csv
 
 # Call data
-echo "SELECT user_id, startdatetime, enddatetime, duration, gest_age, dropreason, callStatus, missedcall_id, media_id, esb_trans_id, tsp_id
+echo "SELECT beneficiary_id user_id, startdatetime, enddatetime, duration, gest_age, dropreason, call_status_id callStatus, missed_call_id missedcall_id, media_id, esb_trans_id, tsp_id
 FROM vw_call_logs
 WHERE startdatetime < ${DATE} AND startdatetime >= date_add(${DATE}, interval -7 day);" | /google/data/ro/projects/speckle/mysql -h 34.93.237.61 -P 3306 -u ${USERNAME} --password=${PASSWORD} mmitrav2 > data/call/call_data.csv
 
