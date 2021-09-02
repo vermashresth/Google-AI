51299fb8d8f14c6abbbc429ad8b53c7d6a19655a
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-09-02 12:43:22 +0000

    Update run.sh

--- mmitra-rmab/run.sh
+++ mmitra-rmab/run.sh
@@ -6,7 +6,11 @@ INTERVENTIONS=$4
 mkdir data
 mkdir data/beneficiary
 mkdir data/call
+chmod 777 get_data.sh
 ./get_data.sh ${USERNAME} ${PASSWORD} ${DATE}
 pip install -r requirements.txt
 unzip policy.zip
 python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS}
+rm -rf data
+chmod 777 insert_interventions.sh
+./insert_interventions.sh ${USERNAME} ${PASSWORD} ${DATE}
