0bb29ab33bd75ac9479c880d4ced27fc0f391cfd
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-09-24 02:43:34 +0530

    Local storage parameter added

--- mmitra-rmab/run.sh
+++ mmitra-rmab/run.sh
@@ -10,7 +10,7 @@ chmod 777 get_data.sh
 ./get_data.sh ${USERNAME} ${PASSWORD} ${DATE}
 pip install -r requirements.txt
 unzip policy.zip
-python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS}
+python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS} 0
 rm -rf data
 chmod 777 insert_interventions.sh
 ./insert_interventions.sh ${USERNAME} ${PASSWORD} ${DATE}
