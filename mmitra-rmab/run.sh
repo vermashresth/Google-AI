a003f1ea4d34e5801ee690e70f3cdf12531eb8f1
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-12-15 09:55:04 +0530

    adding parameter for registration date

--- mmitra-rmab/run.sh
+++ mmitra-rmab/run.sh
@@ -2,15 +2,16 @@ USERNAME=$1
 PASSWORD=$2
 DATE=$3
 INTERVENTIONS=$4
+REG_DATE=$5
 
 mkdir data
 mkdir data/beneficiary
 mkdir data/call
 chmod 777 get_data.sh
-./get_data.sh ${USERNAME} ${PASSWORD} ${DATE}
+./get_data.sh ${USERNAME} ${PASSWORD} ${DATE} ${REG_DATE}
 pip install -r requirements.txt
 unzip policy.zip
-python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS} 0
+python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS} 0 ${REG_DATE}
 rm -rf data
 chmod 777 insert_interventions.sh
 ./insert_interventions.sh ${USERNAME} ${PASSWORD} ${DATE}
