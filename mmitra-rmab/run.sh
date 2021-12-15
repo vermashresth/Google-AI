9eeee751dbcb6906f633d6625c7d8a8bcfc12b26
gargisingh221 <87357170+gargisingh221@users.noreply.github.com>
2021-12-15 11:55:09 +0530

    updating parameters per changes in main file

--- mmitra-rmab/run.sh
+++ mmitra-rmab/run.sh
@@ -11,7 +11,7 @@ chmod 777 get_data.sh
 ./get_data.sh ${USERNAME} ${PASSWORD} ${DATE} ${REG_DATE}
 pip install -r requirements.txt
 unzip policy.zip
-python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS} 0 ${REG_DATE}
+python rmab_individual_clustering.py ${DATE} ${INTERVENTIONS} 0 ${REG_DATE} ${USERNAME} ${PASSWORD}
 rm -rf data
 chmod 777 insert_interventions.sh
 ./insert_interventions.sh ${USERNAME} ${PASSWORD} ${DATE}
