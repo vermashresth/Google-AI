b7e8cf7a0112756d91001ec7f56d565e0f9bc860
Shresth verma <vermashresth@gmail.com>
2021-12-16 12:07:17 +0530

    Clean up

--- mmitra-webapp/prediction/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-from pandarallel import pandarallel
-
-logFormatter = "%(asctime)s - %(levelname)s - %(funcName)s - %(message)s"
-logging.basicConfig(format=logFormatter, level=logging.DEBUG)
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-pandarallel.initialize(verbose=1)
--- mmitra-webapp/prediction/cloudml.py
+++
@@ -1,4 +0,0 @@
-project = "mmitra"
-model = "model1"
-version = "v1"
-ml_service_name = 'projects/{}/models/{}'.format(project, model)
