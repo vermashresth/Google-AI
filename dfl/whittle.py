7807520986d5d8a3705ecfcfbf579f1ca837f1b4
Shresth verma <vermashresth@gmail.com>
2021-12-24 18:39:32 +0530

    vectorized binary search, value iteration

--- dfl/whittle.py
+++ dfl/whittle.py
@@ -1,5 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
+from armman.simulator import get_reward
 import numpy as np
 import pandas as pd
 import pickle
@@ -11,7 +12,12 @@ from itertools import combinations
 """
 DIFFERENTIALBLE WHITTLE INDEX LAYER
 """
-
+def get_reward(R, state, action, m):
+    rewards = np.copy(R[:, state])
+    if not action:
+        rewards += m
+    return rewards
+    
 def newWhittleIndex(P, R, gamma=0.99):
     '''
     Inputs:
@@ -33,30 +39,54 @@ def newWhittleIndex(P, R, gamma=0.99):
     # Part 1: disable gradient tracking and use value iteration and binary search to find Whittle index
     #         This part should support parallelization
     #         This part should always disable tracking gradient of P and R by using "tf.stop_gradient(P)"
-
+    n_actions = 2
+    gamma = 0.99
     N, n_states = P.shape[0], P.shape[1]
     tmp_P, tmp_R = tf.stop_gradient(P), tf.stop_gradient(R)
 
     # initialize upper and lower bounds
-    w_ub = np.ones(N)  # Whittle index upper bound
-    w_lb = np.zeros(N) # Whittle index lower bound
+    w_ub = np.ones((N, n_states))  # Whittle index upper bound
+    w_lb = np.zeros((N, n_states)) # Whittle index lower bound
+    w = (w_ub + w_lb) / 2
 
-    n_binary_search_iterations = 100 # Using a fixed # of iterations or a tolerance rate instead
-    for _ in range(n_binary_search_iteartions):
+    n_binary_search_iters = 20 # Using a fixed # of iterations or a tolerance rate instead
+    n_value_iters = 100
+
+    for _ in range(n_binary_search_iters):
         w = (w_ub + w_lb) / 2
-        # initialize value functions
-        V = np.zeros(N, n_states) # value function
-        for _ in range(iterations):
-            # value iteration to update V
-            pass
-        
+        # initialize value function
+        V = np.zeros((N, n_states))
+        action_max_Q = np.zeros((N, n_states)) # vector to store which of action results in max Q
+        for _ in range(n_value_iters): # value iteration to update V
+            # Reset Q values in every value iteration
+            Q = np.zeros((N, n_states, n_actions)) 
+            # Iterate over state, action, new_state
+            for state in range(n_states):
+                for action in range(n_actions):
+                    for new_state in range(n_states):
+                        # Compute reward given the subsidy
+                        rew = get_reward(tmp_R, state, action, w[:, state]) # vector of size N
+                        # Update Q value for the current state and action
+                        Q[:, state, action] += tmp_P[:, state, action, new_state] * \
+                            (rew + gamma*V[:, new_state])
+                # Update Value by taking max of Q over actions
+                V[:, state] = np.max(Q[:, state, :], axis=1)
+                # Note the action which resulted in max Q value
+                action_max_Q[:, state] = np.argmax(Q[:, state, :], axis=1)
+
         # Compute an indicator vector to mark if Whittle index is too large or too small
         # comparison = (value of not call > value of call) # a vector of size N to indicate if w is too large or not
-        # w_ub = w_ub - (w_ub - w_lb) / 2 * comparison
-        # w_lb = w_lb + (w_ub - w_lb) / 2 * (1 - comparison)
+        comparison = (Q[:, :, 0] > Q[:, :, 1]).astype(int)
+        
+        # Update lower and upper bounds of whittle index binary search
+        w_ub = w_ub - (w_ub - w_lb) / 2 * comparison
+        w_lb = w_lb + (w_ub - w_lb) / 2 * (1 - comparison)
 
     # outcome: w = (w_ub + w_lb) / 2
+    w = (w_ub + w_lb) / 2
 
+
+    # action_max_Q stores the information mentioned below
     # Part 2: figure out which set of argmax in the Bellman equation holds.
     #         V[state] = argmax 0 + w + sum_{next_state} P_passive[state, next_state] V[next_state]
     #                           R[state] + sum_{next_state} P_active[state, next_state] V[next_state]
