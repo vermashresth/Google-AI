131a06c85f7ca90688885fe176a340f9fb76d248
Kai Wang <guaguakai@google.com>
2022-01-20 17:01:57 -0500

    not running df-sim

--- dfl/visualization.py
+++ dfl/visualization.py
@@ -40,8 +40,8 @@ if args.compute:
     print ('Starting seed: ', sd)
     print ('Starting DF Importance Sampling to be saved as: '+DF_IS_filename)
     subprocess.run(f'python3 train.py --method DF --sv {DF_IS_filename} --epochs {args.epochs} --instances {args.instances} --seed {sd} --ope {"IS"}', shell=True)
-    print ('Starting DF Simu based to be saved as: '+DF_SIM_filename)
-    subprocess.run(f'python3 train.py --method DF --sv {DF_SIM_filename} --epochs {args.epochs} --instances {args.instances} --seed {sd} --ope {"sim"}', shell=True)
+    # print ('Starting DF Simu based to be saved as: '+DF_SIM_filename)
+    # subprocess.run(f'python3 train.py --method DF --sv {DF_SIM_filename} --epochs {args.epochs} --instances {args.instances} --seed {sd} --ope {"sim"}', shell=True)
     print ('Starting TS to be saved as: '+TS_filename)
     subprocess.run(f'python3 train.py --method TS --sv {TS_filename} --epochs {args.epochs} --instances {args.instances} --seed {sd}', shell=True)
     print ('ALL THREE DONE')
@@ -65,8 +65,8 @@ if args.plot:
       with open (DF_IS_filename, 'rb') as df_is_file:
           df_is_outputs.append(pickle.load(df_is_file))
 
-      with open (DF_SIM_filename, 'rb') as df_sim_file:
-          df_sim_outputs.append(pickle.load(df_sim_file))
+      # with open (DF_SIM_filename, 'rb') as df_sim_file:
+      #     df_sim_outputs.append(pickle.load(df_sim_file))
 
       with open (TS_filename, 'rb') as ts_file:
           ts_outputs.append(pickle.load(ts_file))
@@ -91,21 +91,21 @@ if args.plot:
     
     for epoch in range(num_epochs):
         df_is_outputs_for_this_epoch=np.array([item[0][mode][epoch] for item in df_is_outputs])
-        df_sim_outputs_for_this_epoch=np.array([item[0][mode][epoch] for item in df_sim_outputs])
+        # df_sim_outputs_for_this_epoch=np.array([item[0][mode][epoch] for item in df_sim_outputs])
         ts_outputs_for_this_epoch=np.array([item[0][mode][epoch] for item in ts_outputs])
 
         df_is_means.append(np.mean(df_is_outputs_for_this_epoch))
-        df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
+        # df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
         ts_means.append(np.mean(ts_outputs_for_this_epoch))
 
         df_is_errors.append(np.std(df_is_outputs_for_this_epoch)/np.sqrt(len(df_is_outputs_for_this_epoch)))
-        df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
+        # df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
         ts_errors.append(np.std(ts_outputs_for_this_epoch)/np.sqrt(len(ts_outputs_for_this_epoch)))
     
     df_is_means=np.array(df_is_means)
     df_is_errors=np.array(df_is_errors)
-    df_sim_means=np.array(df_sim_means)
-    df_sim_errors=np.array(df_sim_errors)
+    # df_sim_means=np.array(df_sim_means)
+    # df_sim_errors=np.array(df_sim_errors)
     
     ts_means=np.array(ts_means)
     ts_errors=np.array(ts_errors)
@@ -134,28 +134,28 @@ if args.plot:
     
     df_is_means=[]
     df_is_errors=[]
-    df_sim_means=[]
-    df_sim_errors=[]
+    # df_sim_means=[]
+    # df_sim_errors=[]
     ts_means=[]
     ts_errors=[]
     
     for epoch in range(num_epochs):
         df_is_outputs_for_this_epoch=np.array([item[1][mode][epoch] for item in df_is_outputs])
-        df_sim_outputs_for_this_epoch=np.array([item[1][mode][epoch] for item in df_sim_outputs])
+        # df_sim_outputs_for_this_epoch=np.array([item[1][mode][epoch] for item in df_sim_outputs])
         ts_outputs_for_this_epoch=np.array([item[1][mode][epoch] for item in ts_outputs])
 
         df_is_means.append(np.mean(df_is_outputs_for_this_epoch))
-        df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
+        # df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
         ts_means.append(np.mean(ts_outputs_for_this_epoch))
 
         df_is_errors.append(np.std(df_is_outputs_for_this_epoch)/np.sqrt(len(df_is_outputs_for_this_epoch)))
-        df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
+        # df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
         ts_errors.append(np.std(ts_outputs_for_this_epoch)/np.sqrt(len(ts_outputs_for_this_epoch)))
     
     df_is_means=np.array(df_is_means)
     df_is_errors=np.array(df_is_errors)
-    df_sim_means=np.array(df_sim_means)
-    df_sim_errors=np.array(df_sim_errors)
+    # df_sim_means=np.array(df_sim_means)
+    # df_sim_errors=np.array(df_sim_errors)
     
     ts_means=np.array(ts_means)
     ts_errors=np.array(ts_errors)
@@ -184,28 +184,28 @@ if args.plot:
     
     df_is_means=[]
     df_is_errors=[]
-    df_sim_means=[]
-    df_sim_errors=[]
+    # df_sim_means=[]
+    # df_sim_errors=[]
     ts_means=[]
     ts_errors=[]
     
     for epoch in range(num_epochs):
         df_is_outputs_for_this_epoch=np.array([item[2][mode][epoch] for item in df_is_outputs])
-        df_sim_outputs_for_this_epoch=np.array([item[2][mode][epoch] for item in df_sim_outputs])
+        # df_sim_outputs_for_this_epoch=np.array([item[2][mode][epoch] for item in df_sim_outputs])
         ts_outputs_for_this_epoch=np.array([item[2][mode][epoch] for item in ts_outputs])
 
         df_is_means.append(np.mean(df_is_outputs_for_this_epoch))
-        df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
+        # df_sim_means.append(np.mean(df_sim_outputs_for_this_epoch))
         ts_means.append(np.mean(ts_outputs_for_this_epoch))
 
         df_is_errors.append(np.std(df_is_outputs_for_this_epoch)/np.sqrt(len(df_is_outputs_for_this_epoch)))
-        df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
+        # df_sim_errors.append(np.std(df_sim_outputs_for_this_epoch)/np.sqrt(len(df_sim_outputs_for_this_epoch)))
         ts_errors.append(np.std(ts_outputs_for_this_epoch)/np.sqrt(len(ts_outputs_for_this_epoch)))
     
     df_is_means=np.array(df_is_means)
     df_is_errors=np.array(df_is_errors)
-    df_sim_means=np.array(df_sim_means)
-    df_sim_errors=np.array(df_sim_errors)
+    # df_sim_means=np.array(df_sim_means)
+    # df_sim_errors=np.array(df_sim_errors)
     
     ts_means=np.array(ts_means)
     ts_errors=np.array(ts_errors)
@@ -233,7 +233,7 @@ if args.plot:
   ### Table information
   ts_selected_metrics = []
   df_is_selected_metrics = []
-  df_sim_selected_metrics = []
+  # df_sim_selected_metrics = []
   for sd in range(args.seed, args.seed+args.tr):
     # Two-stage selected epoch
     print (sd, len(ts_outputs))
@@ -245,18 +245,18 @@ if args.plot:
     df_is_selected_metrics.append([df_is_outputs[sd-args.seed][i]['test'][df_is_selected_epoch] for i in range(3)])
 
     # DF-sim selected epoch
-    df_sim_selected_epoch = np.argmax(df_sim_outputs[sd-args.seed][2]['val'][:-1]) # Maximize SIM OPE
-    df_sim_selected_metrics.append([df_sim_outputs[sd-args.seed][i]['test'][df_sim_selected_epoch] for i in range(3)])
+    # df_sim_selected_epoch = np.argmax(df_sim_outputs[sd-args.seed][2]['val'][:-1]) # Maximize SIM OPE
+    # df_sim_selected_metrics.append([df_sim_outputs[sd-args.seed][i]['test'][df_sim_selected_epoch] for i in range(3)])
   
   ts_selected_metrics=np.array(ts_selected_metrics)
   df_is_selected_metrics=np.array(df_is_selected_metrics)
-  df_sim_selected_metrics=np.array(df_sim_selected_metrics)
+  # df_sim_selected_metrics=np.array(df_sim_selected_metrics)
 
   ts_test_mean, ts_test_ste         = np.mean(ts_selected_metrics, axis=0), np.std(ts_selected_metrics, axis=0) / np.sqrt(len(ts_outputs))
   df_is_test_mean, df_is_test_ste   = np.mean(df_is_selected_metrics, axis=0), np.std(df_is_selected_metrics, axis=0) / np.sqrt(len(df_is_outputs))
-  df_sim_test_mean, df_sim_test_ste = np.mean(df_sim_selected_metrics, axis=0), np.std(df_sim_selected_metrics, axis=0) / np.sqrt(len(df_sim_outputs))
+  # df_sim_test_mean, df_sim_test_ste = np.mean(df_sim_selected_metrics, axis=0), np.std(df_sim_selected_metrics, axis=0) / np.sqrt(len(df_sim_outputs))
 
   print('Random test metrics mean (Loss/IS OPE/Sim OPE): ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$'.format(random_mean[0], random_ste[0], random_mean[1], random_ste[1], random_mean[2], random_ste[2])) # only valid after 0119
   print('Two-stage test metrics mean (Loss/IS OPE/Sim OPE): ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$'.format(ts_test_mean[0], ts_test_ste[0], ts_test_mean[1], ts_test_ste[1], ts_test_mean[2], ts_test_ste[2]))
   print('DF-IS test metrics mean (Loss/IS OPE/ Sim OPE): ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$'.format(df_is_test_mean[0], df_is_test_ste[0], df_is_test_mean[1], df_is_test_ste[1], df_is_test_mean[2], df_is_test_ste[2]))
-  print('DF-sim test metrics mean (Loss/IS OPE/Sim OPE): ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$'.format(df_sim_test_mean[0], df_sim_test_ste[0], df_sim_test_mean[1], df_sim_test_ste[1], df_sim_test_mean[2], df_sim_test_ste[2]))
+  # print('DF-sim test metrics mean (Loss/IS OPE/Sim OPE): ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$, ${:.1f}\pm{:.1f}$'.format(df_sim_test_mean[0], df_sim_test_ste[0], df_sim_test_mean[1], df_sim_test_ste[1], df_sim_test_mean[2], df_sim_test_ste[2]))
