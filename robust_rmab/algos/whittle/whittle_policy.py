3908f1763e9a4fb9094e0dd9a04300c710722ac8
Shresth verma <vermashresth@gmail.com>
2022-01-25 17:25:04 +0530

    super fast whittle computation

--- robust_rmab/algos/whittle/whittle_policy.py
+++ robust_rmab/algos/whittle/whittle_policy.py
@@ -158,6 +158,74 @@ def getWhittleIndex(two_state_probs, timesteps, gamma, sleeping_constraint = Tru
     #return q_values, m_values
     return [m_values[-1], m_values[-2]]
 
+def getFastWhittleIndex(P, gamma):
+    n_actions = 2
+    N, n_states = P.shape[0], P.shape[1]
+
+    R = np.array([[0, 1]]*N)
+    # Just for clarity, add another variable n_wh_states which represents 
+    # the states for which we want to calculate whittle indices. It is same as n_states
+    n_wh_states = n_states
+
+    tmp_P, tmp_R = P, R
+
+    tmp_P = tmp_P.reshape(1, N, n_states, n_actions, n_states).repeat(n_wh_states, axis=0)
+
+    # initialize upper and lower bounds for binary search
+    w_ub = np.ones((n_wh_states, N))  # Whittle index upper bound
+    w_lb = -np.ones((n_wh_states, N)) # Whittle index lower bound
+    w = (w_ub + w_lb) / 2
+
+    n_binary_search_iters = 10 # Using a fixed # of iterations or a tolerance rate instead
+    n_value_iters = 100
+
+    # Run binary search for finding whittle index corresponding to each wh_state
+    for _ in range(n_binary_search_iters):
+        w = (w_ub + w_lb) / 2
+
+        # initialize value function
+        V = np.zeros((n_wh_states, N, n_states))
+        for _ in range(n_value_iters): # value iteration to update V
+
+            # V is originally of shape N x n_states
+            # repeat V to make it of same shape as tmp_P, i.e., N x n_states x n_actions x n_states
+            V_mat = V.reshape((n_wh_states, N, 1, 1, n_states)).repeat(n_states, axis=2).repeat(n_actions, axis=3)
+            
+            # tmp_R is originally of shape N x n_states
+            # repeat reward matrix to make it of same shape as tmp_P
+            rewards_mat = tmp_R.reshape((1, N, n_states, 1, 1)).repeat(n_wh_states, axis=0).repeat(n_actions, axis=3).repeat(n_states, axis=4)
+
+            # w is originally of shape n_states x N
+            # repeat w to make it of shape n_states x N x n_states x n_states to be added to passive action in reward_mat
+            w_mat = w.reshape((n_wh_states, N, 1, 1)).repeat(n_states, axis=2).repeat(n_states, axis=3)
+
+            # Add subsidy to passive action
+            rewards_mat[:, :, :, 0, :]  = rewards_mat[:, :, :, 0, :] + w_mat 
+
+            # Compute discounted future rew
+            out_mat = tmp_P * (rewards_mat + gamma*V_mat)
+            Q = np.sum(out_mat, axis=4) # Q is aggregated over new_state
+            V = np.max(Q, axis=3) # Find V as max over actions
+
+        # Compute an indicator vector to mark if Whittle index is too large or too small
+        # comparison = (value of not call > value of call) # a vector of size N to indicate if w is too large or not
+        passive_q_vals = Q[..., 0]
+        active_q_vals = Q[..., 1]
+        # Return comparision of shape n_states x N
+        comparison = passive_q_vals[np.arange(n_wh_states).reshape(-1, 1).repeat(N, axis=1),
+                                    np.arange(N).reshape(1, -1).repeat(n_wh_states, axis=0),
+                                    np.arange(n_wh_states).reshape(-1, 1).repeat(N, axis=1)] > \
+                     active_q_vals[np.arange(n_wh_states).reshape(-1, 1).repeat(N, axis=1),
+                                    np.arange(N).reshape(1, -1).repeat(n_wh_states, axis=0),
+                                    np.arange(n_wh_states).reshape(-1, 1).repeat(N, axis=1)]  
+            
+        # TODO: Might want to update whittle indices for only those (arms, states) having abs(q_diff) more than a Q_delta  
+        # Update lower and upper bounds of whittle index binary search
+        w_ub = w_ub - (w_ub - w_lb) / 2 * comparison
+        w_lb = w_lb + (w_ub - w_lb) / 2 * (1 - comparison)
+    w = (w_ub + w_lb) / 2
+    return w.T
+
 
 def getTopk(a, k):
     '''
@@ -215,8 +283,9 @@ class WhittlePolicy():
         # self.whittle_indices = np.array([getWhittleIndex(self.T[i], self.horizon, self.gamma)\
         #      for i in range(self.N)])
         print('Computing Whittle Indices!!!')
-        self.whittle_indices = np.array([getWhittleIndex(self.cluster_T[i], self.horizon, self.gamma)\
-             for i in tqdm.tqdm(range(self.n_clusters))])
+        # self.whittle_indices = np.array([getWhittleIndex(self.cluster_T[i], self.horizon, self.gamma)\
+        #      for i in tqdm.tqdm(range(self.n_clusters))])
+        self.whittle_indices = getFastWhittleIndex(self.cluster_T, self.gamma)
 
         # self.whittle_indices = np.random.uniform(0, 1*np.sum(a_nature), (self.N, self.S))
 
@@ -242,8 +311,11 @@ class WhittlePolicy():
         whittle_indices = cluster_whittle_index[np.arange(self.n_arms), observations_array]
         actions = np.zeros(self.n_arms)
         actions[getTopk(whittle_indices, int(actual_B))] = 1
-        self.current_lamb = (whittle_indices[getTopk(whittle_indices, int(self.B))[-1]]+\
-            whittle_indices[getTopk(whittle_indices, int(self.B))[-2]])/2
+        try:
+            self.current_lamb = (whittle_indices[getTopk(whittle_indices, int(self.B))[-1]]+\
+                whittle_indices[getTopk(whittle_indices, int(self.B))[-2]])/2
+        except:
+            self.current_lamb = 0
         actions = actions.astype(int)
         assert np.sum(actions)==actual_B
         return actions
